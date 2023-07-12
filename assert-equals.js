eec08dadc34b4016ace59a795b3e89cd751d6789
Anton Novak <anton-novak@outlook.com>
2023-07-12 18:07:24 +0100

    feat: additional tests, assertEquals function

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,70 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+    if (arguments.length === 1) {
+        throw new Error('Actual value to compare missing in the arguments');
+    };
+    if (arguments.length === 0) {
+        throw new Error('Expected and actual values missing in the arguments');
+    };
+
+    function checkUnsupported(arg) {
+        if (typeof arg === 'object' && arg !== null) {
+            throw new Error(`Argument ${arg} of type ${typeof arg} unsupported`);
+        };
+        if (typeof arg === 'function') {
+            throw new Error(`Argument ${arg} of type ${typeof arg} unsupported`);
+        };
+    };
+
+    Array.from(arguments).forEach(arg => {
+        if (Array.isArray(arg)) {
+            arg.forEach((element) => checkUnsupported(element));
+        } else {
+            checkUnsupported(arg);
+        };
+    });
+
+    if (typeof expect !== typeof actual) {
+        if (
+            typeof expect === 'undefined' && actual === null ||
+            typeof actual === 'undefined' && expect === null
+            ) {
+            throw new Error(`You are comparing ${typeof expect === 'undefined' ? 'undefined (expect)' : 'null (expect)'} 
+            with ${typeof actual === 'undefined' ? 'undefined (actual)' : 'null (actual)'}`);
+        } else {
+            throw new Error(`Expected type ${typeof expect} but found type ${typeof actual}`);
+        };
+    };
+    
+    
+    if (
+        Array.isArray(expect) && actual === null ||
+        Array.isArray(actual) && expect === null
+        ) {
+        throw new Error(`Expected type ${typeof expect === null ? 'object (null)' : 'object (array)'} but found type ${typeof actual === null ? 'object (null)' : 'object (array)'}`);
+    };
+
+    function comparePrimitives(first, second) {
+        if (first === second) {
+            return;
+        } else {
+            throw new Error(`Expected ${first} but found ${second}`);
+        };
+    };
+
+    if (Array.isArray(expect) && Array.isArray(actual)) {
+        if (expect.length !== actual.length) {
+            throw new Error(`Expected array length ${expect.length} but found ${actual.length}`);
+        } else {
+            for (let i = 0; i < expect.length; i++) {
+                comparePrimitives(expect[i], actual[i]);
+            };
+        };
+        return;
+    };
+
+    comparePrimitives(expect, actual);
+}
+
+assertEquals(['a', 'b'], ['a', 'd']);
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,72 @@
 const assertEquals = require('./assert-equals')
 
 describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
+  describe('throws a specific error when', () => {
+    it('given insufficient arguments', () => {
+      expect(() => assertEquals()).toThrow('missing');
+      expect(() => assertEquals(1)).toThrow('missing');
+    })
+    it('given unsupported arguments (non-primitives excl. simple arrays)', () => {
+      expect(() => assertEquals({ a: 1 }, 1)).toThrow('unsupported');
+      expect(() => assertEquals({ a: 1 }, { b: 2 })).toThrow('unsupported');
+      expect(() => assertEquals([1, { a: 1 }], 3)).toThrow('unsupported');
+      expect(() => assertEquals(4, () => undefined)).toThrow('unsupported');
     })
   })
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
+  describe('returns without an error when expected and actual are the same:', () => {
+    it('number', () => {
+      expect(() => assertEquals(3, 3)).not.toThrow()
+    })
+    it('string', () => {
+      expect(() => assertEquals('abc', 'abc')).not.toThrow()
+    })
+    it('boolean', () => {
+      expect(() => assertEquals(false, false)).not.toThrow()
+    })
+    it('undefined or null', () => {
+      expect(() => assertEquals(null, null)).not.toThrow()
+      expect(() => assertEquals(undefined, undefined)).not.toThrow()
+    })
+    it('array', () => {
+      expect(() => assertEquals([1, 2, 3], [1, 2, 3])).not.toThrow()
+    })
+  })
+
+  describe('throws an error when expected and actual are different:', () => {
+    it('types (with a type message)', () => {
+      expect(() => assertEquals(1, 'hello')).toThrow('Expected type');
+    })
+    it('types (for arrays and nulls)', () => {
+      expect(() => assertEquals([1, 2, 3], null)).toThrow('Expected type');
+    })
+    it('numbers', () => {
+      expect(() => assertEquals(3, 11)).toThrow()
+    })
+    it('strings', () => {
+      expect(() => assertEquals('abc', 'xyz')).toThrow()
+    })
+    it('booleans', () => {
+      expect(() => assertEquals(true, false)).toThrow()
+    })
+    it('arrays', () => {
+      expect(() => assertEquals([1, 2, 3], [1, 2, 3, 4])).toThrow()
+      expect(() => assertEquals([1, 2, 3], ['a', 'b', 'c'])).toThrow()
+    })
+  })
+
+  describe('if arrays are compared', () => {
+    it('throws a specific error re length', () => {
+      expect(() => assertEquals([1, 2, 3], ['a', 'b', 'c', 'd'])).toThrow('length')
+    })
+  })
+
+  describe('if null and undefined are compared', () => {
+    it('throws a specific error', () => {
+      expect(() => assertEquals(null, undefined)).toThrow('null')
+      expect(() => assertEquals(null, undefined)).toThrow('undefined')
+      expect(() => assertEquals(undefined, null)).toThrow('null')
+      expect(() => assertEquals(undefined, null)).toThrow('undefined')
+    })
   })
 })
